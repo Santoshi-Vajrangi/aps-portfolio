f3df865231cfdb9977884f80c61a806a2142ab26
Santoshi-Vajrangi <143388470+Santoshi-Vajrangi@users.noreply.github.com>
2024-11-13 17:34:56 +0530

    Update Kruskals.cpp

--- codes/Kruskals.cpp
+++ codes/Kruskals.cpp
@@ -1,121 +1,75 @@
-#include <stdio.h>
-#define EDGES 14
-#define VERTICES 9
-int arr[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8}; // initialize
-struct graph
-{
-    int u; // the starting point of edge
-    int v; // the ending of edge
-    int w; // weight on edge
-};
-int cost;
-struct graph G[EDGES] =
-    {
-        {6, 7, 2},
-        {2, 8, 4},
-        {5, 6, 4},
-        {0, 1, 8},
-        {2, 5, 8},
-        {6, 8, 12},
-        {2, 3, 14},
-        {7, 8, 14},
-        {0, 7, 16},
-        {1, 2, 16},
-        {3, 4, 18},
-        {4, 5, 20},
-        {1, 7, 22},
-        {3, 5, 28}};
-void sort()
-{
-    struct graph temp;
-    for (int i = 0; i < EDGES - 1; i++)
-    {
-        for (int j = 0; j < EDGES - i - 1; j++)
-        {
-            if (G[j].w > G[j + 1].w)
-            {
-                temp = G[j];
-                G[j] = G[j + 1];
-                G[j + 1] = temp;
-            }
-        }
-    }
-}
-// void Union(int u, int v)
-// {
-//     for (int i = 0; i < VERTICES; i++)
-//     {
-//         if (arr[i] == u)
-//         {
-//             arr[i] = v;
-//         }
-//     }
-// }
-// int find(int u, int v)
-// {
-//     int i = arr[u];
-//     int j = arr[v];
-//     if (i != j)
-//     {
-//         printf("(%d , %d)\n", u, v);
-//         Union(i, j);
-//         return 1;
-//     }
-//     else
-//     {
-//         printf("discard\n");
-//         return 0;
-//     }
-// }
-int root(int i)
-{
-    while (i != arr[i])
-    {
-        i = arr[i];
-    }
-    return i;
-}
-void Union(int u, int v)
-{
-    int ru = root(u);
-    int rv = root(v);
-    arr[ru] = rv;
-}
-int find(int u, int v)
-{
-    if (root(u) != root(v))
-    {
-        printf("(%d , %d)\n", u, v);
-        Union(root(u), root(v));
-        return 1;
-    }
-    else
-    {
-        printf("discard\n");
-        return 0;
-    }
-}
-int main()
-{
-    sort();
-    for (int i = 0; i < EDGES; i++)
-    {
-        printf("%d %d %d\n", G[i].u, G[i].v, G[i].w);
-    }
+/*
+	Notes:
+	1- It used edge list 
+	2- Sort all edges according to the weights
+	3- Then start selecting edges 1 by 1 
+	4- Only select an edge if it doesnt form a cycle 
+    6- Time Complexity O(ElogV)
+	7- Space Complexity O(V)
+    5-  Purpose: Finds the Minimum Spanning Tree (MST) in an undirected, weighted graph. The MST connects all vertices
+       with the minimum possible total edge weight, without forming cycles.
 
-    int count = 0;
-    for (int i = 0; i < EDGES; i++)
+*/ 
+
+
+
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+ 
+    static bool comp(vector<int>&a, vector<int>&b)
+	{
+	    return a[2] < b[2];
+	}
+	
+	int findparent(int n, vector<int>&parent)
+	{
+	    if(parent[n] == n)
+	      return n;
+	    int x = findparent(parent[n], parent);
+	    return x;
+	}
+	
+    int spanningTree(int v, vector<vector<int>> adj[])
     {
-        int res = find(G[i].u, G[i].v);
-        if (res == 1)
+        
+         vector<vector<int>>arr;
+        for(int i=0; i<v; i++)
         {
-            cost += G[i].w;
-            count++;
-            if (count == VERTICES - 1)
+            for(x: adj[i])
             {
-                break;
+                int neibh = x[0];
+                int wt = x[1];
+                
+                vector<int>temp;
+                temp.push_back(i);
+                temp.push_back(neibh);
+                temp.push_back(wt);
+                arr.push_back(temp);
             }
         }
+        
+        sort(arr.begin(), arr.end(), comp);
+        
+        vector<int>parent(v, 0);
+        for(int i=0; i<v; i++)
+           parent[i] = i;
+           
+        int sum = 0;
+        for(int i=0; i<arr.size(); i++)
+        {
+            int x = findparent(arr[i][0], parent);
+            int y = findparent(arr[i][1], parent);
+            if(x != y)
+            {
+                sum = sum + arr[i][2];
+                parent[max(x, y)] = min(x, y);
+            }
+        }
+        return sum;
     }
-    printf("MST cost : %d\n", cost);
-}
+    
+    
+};
